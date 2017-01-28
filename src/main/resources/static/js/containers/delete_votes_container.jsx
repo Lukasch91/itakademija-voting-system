3b8bac386239ef70c670cb5982a9baebdad15c35
Mindaugas <mindaugas.dziautas@gmail.com>
2017-01-28 16:04:52 +0200

    single-member election votes can be deleted

---
+++ src/main/resources/static/js/containers/delete_votes_container.jsx
@@ -0,0 +1,55 @@
+var DeleteVotesContainer = React.createClass({
+
+    getInitialState: function() {
+        return {
+            constituencies: [],
+            districts: [],
+            districtVal: 'base'
+        };
+    },
+    
+    componentWillMount: function() {
+        var self = this;
+        axios.get('/api/constituency')
+        .then(function (response) {
+            self.setState({ 
+                constituencies: response.data 
+            });
+        });
+        axios.get('/api/district')
+        .then(function (response) {
+            self.setState({ 
+                districts: response.data
+            });
+        });
+    },
+    
+    handleFieldChange: function(event) {
+        this.setState({districtVal: event.target.value});
+    },
+    
+    handleDeleteVotes: function() {
+        if (this.state.districtVal != 'base') {
+        axios.delete('/api/singleelectiondistrict/' + this.state.districtVal).then(function() {
+            console.log('votes deleted');
+        });
+        } else {
+            alert('Paršome pasirinkti apygardą');
+        }
+    },
+    
+    
+    render: function() {
+        return (
+
+                <DeleteVotesComponent  
+                constituencies={this.state.constituencies}  
+                districts={this.state.districts}
+                onFieldChange={this.handleFieldChange}
+                onDeleteVotes={this.handleDeleteVotes}/>
+
+                )
+  }
+});
+
+window.DeleteVotesContainer = DeleteVotesContainer;
