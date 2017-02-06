86325adeac046bc164b20f72bfcbe250b8bdca50
Arnoldas <arnoldas.urbelis@gmail.com>
2017-02-06 22:15:24 +0200

    candidate controller uploads csv

--- src/main/java/vs/Application.java
+++ src/main/java/vs/Application.java
@@ -1,7 +1,9 @@
 package vs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;//file upload
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,11 +15,24 @@ import springfox.documentation.service.ApiInfo; //swagger
 import springfox.documentation.spi.DocumentationType; //swagger
 import springfox.documentation.spring.web.plugins.Docket; //swagger
 import springfox.documentation.swagger2.annotations.EnableSwagger2; //swagger
+import vs.admin.features.candidate.model.storage.StorageProperties;// file upload
+import vs.admin.features.candidate.model.storage.StorageService;// file upload
 
 @EnableSwagger2 // swagger
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class) // file upload
 public class Application {
 
+	/* ===== File upload bean===== */
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+	/* ===== File upload bean===== */
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurerAdapter() {
