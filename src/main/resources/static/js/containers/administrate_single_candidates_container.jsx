ca48de5c1313faf7d64ac8e1af5c531fd3863bf4
ArnoldasUrbelis <arnoldas.urbelis@gmail.com>
2017-02-07 13:06:50 +0200

    District Candidate React update

--- src/main/resources/static/js/containers/administrate_single_candidates_container.jsx
+++ src/main/resources/static/js/containers/administrate_single_candidates_container.jsx
@@ -1,31 +1,58 @@
-var AdministrateSingleCandidatesContainer = React.createClass({
+var AdministrateSingleCandidatesContainer = React.createClass( {
 
-    
+
     getInitialState: function() {
         return {
-            constituencies: [] 
+            constituencies: []
         };
     },
-    
+
     componentWillMount: function() {
         var self = this;
-        axios.get('/api/constituency')
-        .then(function (response) {
-            self.setState({ 
-                constituencies: response.data 
+        axios.get( '/api/constituency' )
+            .then( function( response ) {
+                self.setState( {
+                    constituencies: response.data
+                });
             });
-        });
 
     },
-    
-    
+    //===========================================================
+    handleAddDistrictCandidates: function( e ) {
+        e.preventDefault();
+        var self = this;
+
+
+
+
+//
+//        axios.post( '/api/representative', {
+//            name: this.state.representative.name,
+//            surname: this.state.representative.surname,
+//            loginName: this.state.representative.loginName,
+//            password: this.state.representative.password,
+//            email: this.state.representative.email,
+//            districtId: this.props.params.disId
+//
+//        })
+//            .then( function( response ) {
+//                success = 1;
+//                console.log( response );
+//                console.log( "representative added" );
+//            })
+
+    },
+    //===========================================================
+
     render: function() {
         return (
-        <div>
-        <AdministrateSingleCandidatesComponent constituencies={this.state.constituencies} />
-        </div>
+            <div>
+                <AdministrateSingleCandidatesComponent
+                    constituencies={this.state.constituencies}
+                    onAddDistrictCandidates={this.handleAddDistrictCandidates} />
+            </div>
         )
-  }
+    }
 });
 
 window.AdministrateSingleCandidatesContainer = AdministrateSingleCandidatesContainer;
