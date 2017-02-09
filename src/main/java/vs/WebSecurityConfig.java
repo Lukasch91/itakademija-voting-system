43dcbfdae7d6f4ca84b6ba484f76afd59e03cbb6
mindaugasd <mindaugas.dziautas@gmail.com>
2017-02-09 10:49:23 +0200

    bug fixed

--- src/main/java/vs/WebSecurityConfig.java
+++ src/main/java/vs/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.csrf().disable()
 		.headers().disable()
 		.authorizeRequests()
-			.antMatchers(/*"/", "/js/**", */"/api/**", "/", "/index.html", "/index_public.html").permitAll()
+			.antMatchers(/*"/", "/js/**", */"/api/**", "/", "/index.html", "/js/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 		.formLogin()
