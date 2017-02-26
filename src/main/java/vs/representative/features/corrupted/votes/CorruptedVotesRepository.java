611c885dfb60f3df2c295d0d80818f34e62a6e76
Lukas <lukas.cholomskis@gmail.com>
2017-02-26 22:13:14 +0200

    bug fix

--- src/main/java/vs/representative/features/corrupted/votes/CorruptedVotesRepository.java
+++ src/main/java/vs/representative/features/corrupted/votes/CorruptedVotesRepository.java
@@ -16,7 +16,7 @@ public class CorruptedVotesRepository {
 
 	private static final String FIND_BY_TYPE = "SELECT e FROM CorruptedVotes e WHERE deleted_date is null AND typeMulti IS ";
 	private static final String FIND_BY_CONSTITUENCY_ID = "SELECT sum(c.votes) from CorruptedVotes c LEFT JOIN c.district cd WHERE cd.constituencyId = :id and c.typeMulti ='false'";
-	private static final String FIND_FIND_BY_DISTRICT_ID = "SELECT sum(c.votes) FROM CorruptedVotes c left join c.district cd WHERE cd.id =:id and c.typeMult ='false'";
+	private static final String FIND_FIND_BY_DISTRICT_ID = "SELECT sum(c.votes) FROM CorruptedVotes c left join c.district cd WHERE cd.id =:id and c.typeMulti ='false'";
 
 	private static final String FIND_ALL_MULTI_INVALID_VOTES = "SELECT sum(c.votes) FROM CorruptedVotes c WHERE c.published_date is not null and c.deleted_date is null and c.typeMulti='true'";
 
