2ad117b102313eb6a0b367a37846560efaa62a76
Arnoldas <arnoldas.urbelis@gmail.com>
2017-03-12 01:33:33 +0200

    CSV export update

--- src/main/java/vs/public_/download/results/DownloadResultsService.java
+++ src/main/java/vs/public_/download/results/DownloadResultsService.java
@@ -5,11 +5,13 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import vs.public_.multi.results.MultiElectionConstituencyList;
+import vs.public_.multi.results.MultiElectionDistrictList;
+import vs.public_.multi.results.MultiElectionResults;
+import vs.public_.multi.results.MultiElectionResultsService;
 import vs.public_.single.results.SingleElectionConstituency;
 import vs.public_.single.results.SingleElectionDistrict;
 import vs.public_.single.results.SingleElectionResult;
@@ -26,7 +28,9 @@ public class DownloadResultsService {
 
 	@Autowired
 	SingleElectionResultsService singleElectionConstitencyService;
-
+	@Autowired
+	MultiElectionResultsService multiElectionResultsService;
+	
 	public String returnSelector(Integer request) {
 
 		log.info("||--> was used." );
@@ -36,10 +40,166 @@ public class DownloadResultsService {
 			return request1();
 		case 2:
 			return request2();
+		case 3:
+			return request3();
+		case 4:
+			return request4();
 		default:
 			return null;
 		}
+	}
 
+	/**
+	 * Method prepares all the data of multi elections as an MultiElectionResultsAll object List. For
+	 * that it assembles MultiElectionConstituencyList, MultiElectionDistrictList and
+	 * MultiElectionResults objects into one object.
+	 * 
+	 * @return
+	 */
+	public String request4() {
+		log.info("||--> Started..." );
+		List<MultiElectionResultsAll> all = new ArrayList<MultiElectionResultsAll>();
+
+		List<MultiElectionConstituencyList> multiElectionConstituencyList = multiElectionResultsService
+				.getMultiElectionConstituencyList();
+
+		// =================================================
+		for (MultiElectionConstituencyList mECL : multiElectionConstituencyList) {
+
+			List<MultiElectionDistrictList> multiElectionDistrictList =  multiElectionResultsService
+					.getResultsOfDistricts(mECL.getId());
+
+			for (MultiElectionDistrictList mEDL : multiElectionDistrictList) {
+
+				List<MultiElectionResults> multiElectionResults = multiElectionResultsService
+						.getDistrictPartiesResults(mEDL.getId());
+
+				for (MultiElectionResults mER : multiElectionResults) {
+
+					MultiElectionResultsAll r = new MultiElectionResultsAll();
+					/* constituency */
+					r.setConId(mECL.getId());
+					r.setConTitle(mECL.getTitle());
+					r.setConNumberOfDistricts(mECL.getNumberOfDistricts());
+					r.setConDistrictsPublishedResults(mECL.getDistrictsPublishedResults());
+					r.setConNumberOfVoters(mECL.getNumberOfVoters());
+					r.setConNumberOfVotersWhoVoted(mECL.getNumberOfVotersWhoVoted());
+					r.setConPercentegeOfVoted(mECL.getPercentegeOfVoted());
+					r.setConInvalidVotes(mECL.getInvalidVotes());
+					r.setConPercentageOfInvalidVotes(mECL.getPercentageOfInvalidVotes());
+					r.setConValidVotes(mECL.getValidVotes());
+					r.setConPercentageOfValidVotes(mECL.getPercentageOfValidVotes());
+					/* constituency */
+					/* district */				
+					r.setDistId(mEDL.getId());
+					r.setDistTitle(mEDL.getTitle());
+					r.setDistNumberOfVoters(mEDL.getNumberOfVoters());
+					r.setDistNumberOfVotersWhoVoted(mEDL.getNumberOfVotersWhoVoted());
+					r.setDistPercentageOfVoted(mEDL.getPercentageOfVoted());
+					r.setDistInvalidVotes(mEDL.getInvalidVotes());
+					r.setDistPercentageOfInvalidVotes(mEDL.getPercentageOfInvalidVotes());
+					r.setDistValidVotes(mEDL.getValidVotes());
+					r.setDistPercentageOfValidVotes(mEDL.getPercentageOfValidVotes());
+					/* district */
+					/* result */
+					r.setResId(mER.getId());
+					r.setResPartyTitle(mER.getPartyTitle());
+					r.setResVotes(mER.getVotes());
+					r.setResPercentageOfAllVotes(mER.getPercentageOfAllVotes());
+					r.setResNumberOfMandates(mER.getNumberOfMandates());
+					/* result */
+					all.add(r);
+				}
+			}
+		}
+		// =================================================
+
+		List<String[]> tableData = new ArrayList<String[]>();
+		tableData.add(request4Header());
+		for (MultiElectionResultsAll sERA : all) {
+			tableData.add(request4ToStringArray(sERA));
+		}
+		String result = listArrayToCSVSting(tableData);
+		log.info("||--> Finished!" );
+		return result;
+	}
+	
+	/**
+	 * Method prepares all the data of single elections as an SingleElectionResultsAll object List. For
+	 * that it assembles SingleElectionConstituency, SingleElectionDistrict and
+	 * SingleElectionResult objects into one object.
+	 * 
+	 * @return
+	 */
+	public String request3() {
+		log.info("||--> Started..." );
+		List<SingleElectionResultsAll> all = new ArrayList<SingleElectionResultsAll>();
+
+		List<SingleElectionConstituency> singleConstituencyResultsList = singleElectionConstitencyService
+				.singleElectionConstituencyResults();
+
+		// =================================================
+		for (SingleElectionConstituency sEC : singleConstituencyResultsList) {
+
+			List<SingleElectionDistrict> singleDistrictRelsultsByConsId = singleElectionConstitencyService
+					.singleElectionDistrictResults(sEC.getConstituencyId());
+
+			for (SingleElectionDistrict sED : singleDistrictRelsultsByConsId) {
+
+				List<SingleElectionResult> singleRelsultsByDistId = singleElectionConstitencyService
+						.getSingleElectionResultInDistrict(sED.getDistrictId());
+
+				for (SingleElectionResult sER : singleRelsultsByDistId) {
+
+					SingleElectionResultsAll r = new SingleElectionResultsAll();
+					/* constituency */
+					r.setConId(sEC.getConstituencyId());
+					r.setConTitle(sEC.getTitle());
+					r.setConNumberOfDistricts(sEC.getNumberOfDistricts());
+					r.setConNumberOfDistrictsPublishedResults(sEC.getNumberOfDistrictsPublishedResults());
+					r.setConNumberOfVoters(sEC.getNumberOfVoters());
+					r.setConNumberOfVotersWhoVote(sEC.getNumberOfVotersWhoVote());
+					r.setConPercentageOfVotersWhoVote(sEC.getPercentageOfVotersWhoVote());
+					r.setConNumberOfInvalidVotes(sEC.getNumberOfInvalidVotes());
+					r.setConPercentageOfInvalidVotes(sEC.getPercentageOfInvalidVotes());
+					r.setConNumberOfValidVotes(sEC.getNumberOfValidVotes());
+					r.setConPercentageOfValidVotes(sEC.getPercentageOfValidVotes());					
+					/* constituency */
+					/* district */
+					r.setDistId(sED.getDistrictId());
+					r.setDistTitle(sED.getTitle());
+					r.setDistNumberOfVoters(sED.getNumberOfVoters());
+					r.setDistVoted(sED.getVoted());
+					r.setDistPercentageOfVoted(sED.getPercentageOfVoted());
+					r.setDistInvalidVotes(sED.getInvalidVotes());
+					r.setDistPercentageOfInvalidVotes(sED.getPercentageOfInvalidVotes());
+					r.setDistValidVotes(sED.getValidVotes());
+					r.setDistPercentageOfValidVotes(sED.getPercentageOfValidVotes());
+					/* district */
+					/* result */
+					r.setResCandidateId(sER.getCandidateId());
+					r.setResCandidateFirstname(sER.getCandidateFirstname());
+					r.setResCandidateSurname(sER.getCandidateSurname());
+					r.setResParty(sER.getParty());
+					r.setResVoted(sER.getVoted());
+					r.setResPercentageOfValidVotes(sER.getPercentageOfValidVotes());
+					r.setResPercentageOfAllVotes(sER.getPercentageOfAllVotes());
+					r.setResConsId(sER.getConsId());
+					/* result */
+					all.add(r);
+				}
+			}
+		}
+		// =================================================
+
+		List<String[]> tableData = new ArrayList<String[]>();
+		tableData.add(request3Header());
+		for (SingleElectionResultsAll sERA : all) {
+			tableData.add(request3ToStringArray(sERA));
+		}
+		String result = listArrayToCSVSting(tableData);
+		log.info("||--> Finished!" );
+		return result;
 	}
 
 	public String request2() {
@@ -48,8 +208,6 @@ public class DownloadResultsService {
 
 		List<SingleElectionConstituency> singleConstituencyResultsList = singleElectionConstitencyService
 				.singleElectionConstituencyResults();
-
-		// =================================================
 
 		for (SingleElectionConstituency sEC : singleConstituencyResultsList) {
 
@@ -72,18 +230,14 @@ public class DownloadResultsService {
 					r.setResVoted(sER.getVoted());
 					r.setResPercentageOfValidVotes(sER.getPercentageOfValidVotes());
 					r.setResPercentageOfAllVotes(sER.getPercentageOfAllVotes());
-
 					all.add(r);
 				}
 			}
 		}
-
-		// =================================================
-
 		List<String[]> tableData = new ArrayList<String[]>();
-		tableData.add(singleElectionResultsAllHeader());
+		tableData.add(request2Header());
 		for (SingleElectionResultsAll sERA : all) {
-			tableData.add(singleElectionResultsAllToStringArray(sERA));
+			tableData.add(request2ToStringArray(sERA));
 		}
 		String result = listArrayToCSVSting(tableData);
 		log.info("||--> Finished!" );
@@ -92,14 +246,13 @@ public class DownloadResultsService {
 
 	private String request1() {
 		log.info("||--> Started..." );
-		
 		List<SingleElectionConstituency> singleConstituencyResultsList = singleElectionConstitencyService
 				.singleElectionConstituencyResults();
 
 		List<String[]> tableData = new ArrayList<String[]>();
 
 		for (SingleElectionConstituency sEC : singleConstituencyResultsList) {
-			tableData.add(singleElectionConstituencyToStringArray(sEC));
+			tableData.add(request1ToStringArray(sEC));
 		}
 
 		String result = listArrayToCSVSting(tableData);
@@ -107,13 +260,183 @@ public class DownloadResultsService {
 		return result;
 	}
 
-	public String[] singleElectionResultsAllHeader() {
+	private List<String[]> dataLimiter(List<String[]> listToLimit , int[] columnsToRemove) {
+		
+		for(String[] row : listToLimit) {	
+		}
+//		public static String[] removeElements(String[] input, String deleteMe) {
+//		    List result = new LinkedList();
+//
+//		    for(String item : input)
+//		        if(!deleteMe.equals(item))
+//		            result.add(item);
+//
+//		    return result.toArray(input);
+//		}		
+		return null;
+	}
+	
+	public String[] request4Header() {
+		log.info("||--> was used." );
+		return new String[] {
+				/* constituency */			
+				"Apygardos Id",
+				"Apygardos pavadinimas",
+				"Apygardos apylinkiu skaicius",
+				"Apygardos apylinkes paskelbusios rezultatus",
+				"Apygardos balsuotoju skaicius",
+				"Apygardos balsavusiuju skaicius",
+				"Apygardos balsavusiuju % nuo apygardos balsuotoju",
+				"Apygardos sugadinti balsai",
+				"Apygardos sugadintu balsu % nuo apygarodos balsuotoju",
+				"Apygardos geru balsu skaicius",
+				"Apygardos geru balsu % nuo apygardos balsuotoju",
+				/* constituency */
+				/* district */
+				"Apylinkes Id",
+				"Apylinkes pavadinimas",
+				"Apylinkes balsuotoju skaicius",
+				"Apylinkes balsavusiuju skaicius",
+				"Apylinkes balsavusiuju % nuo apylinkes balsuotoju",
+				"Apylinkes sugadinti balsai",
+				"Apylinkes sugadintu balsu % nuo apylinkes balsuotoju",
+				"Apylinkes geru balsu skaicius",
+				"Apylinkes geru balsu % nuo apylinkes balsuotoju",
+				/* district */
+				/* result */
+				"Partijos Id",
+				"Partijos pavadinimas",
+				"Partijos balsu skaicius",
+				"Partijos geru balsu % nuo apylinkes balsuotoju???"
+//				"Partijos mandatai"
+				/* result */	
+				};	
+	}
+	
+	public String[] request3Header() {
+		log.info("||--> was used." );
+		return new String[] {
+				/* constituency */				
+				"Apygardos Id",
+				"Apygardos pavadinimas",
+				"Apygardos apylinkiu skaicius",
+				"Apygardos apylinkes paskelbusios rezultatus",
+				"Apygardos balsuotoju skaicius",
+				"Apygardos balsavusiuju skaicius",
+				"Apygardos balsavusiuju % nuo apygardos balsuotoju",
+				"Apygardos sugadinti balsai",
+				"Apygardos sugadintu balsu % nuo apygarodos balsuotoju",
+				"Apygardos geru balsu skaicius",
+				"Apygardos geru balsu % nuo apygardos balsuotoju",
+				/* constituency */
+				/* district */
+				"Apylinkes Id",
+				"Apylinkes pavadinimas",
+				"Apylinkes balsuotoju skaicius",
+				"Apylinkes balsavusiuju skaicius",
+				"Apylinkes balsavusiuju % nuo apylinkes balsuotoju",
+				"Apylinkes sugadinti balsai",
+				"Apylinkes sugadintu balsu % nuo apylinkes balsuotoju",
+				"Apylinkes geru balsu skaicius",
+				"Apylinkes geru balsu % nuo apylinkes balsuotoju",
+				/* district */
+				/* result */
+				"Kandidato Id",
+				"Kandidato vardas",
+				"Kandidato pavarde",
+				"Kandidato partija",
+				"Kandidato balsu skaicius",
+				"Kandidato geru balsu % nuo apylinkes balsuotoju???",
+				"Kandidato geru balsu % nuo visu balsuotoju???",
+				"Kandidato Apygardos Id",
+				/* result */	
+				};	
+	}
+	
+	public String[] request2Header() {
 		log.info("||--> was used." );
 		return new String[] { "Apygarda", "Apylinkė", "Kandidato vardas", "Kandidato pavardė", "Kandidato partija",
 				"Balsavo rinkėjų", "Gerų balsų %", "Visų balsų % ??? :D" };
 	}
 
-	public String[] singleElectionResultsAllToStringArray(SingleElectionResultsAll singleElectionResultsAll) {
+	public String[] request4ToStringArray(MultiElectionResultsAll multiElectionResultsAll) {
+		log.info("||--> was used." );
+		return new String[] { 
+				/* constituency */
+				multiElectionResultsAll.getConId().toString(),
+				multiElectionResultsAll.getConTitle(),
+				multiElectionResultsAll.getConNumberOfDistricts().toString(),
+				multiElectionResultsAll.getConDistrictsPublishedResults().toString(),
+				multiElectionResultsAll.getConNumberOfVoters().toString(),
+				multiElectionResultsAll.getConNumberOfVotersWhoVoted().toString(),
+				multiElectionResultsAll.getConPercentegeOfVoted().toString(),
+				multiElectionResultsAll.getConInvalidVotes().toString(),
+				multiElectionResultsAll.getConPercentageOfInvalidVotes().toString(),
+				multiElectionResultsAll.getConValidVotes().toString(),
+				multiElectionResultsAll.getConPercentageOfValidVotes().toString(),
+				/* constituency */
+				/* district */
+				multiElectionResultsAll.getDistId().toString(),
+				multiElectionResultsAll.getDistTitle(),
+				multiElectionResultsAll.getDistNumberOfVoters().toString(),
+				multiElectionResultsAll.getDistNumberOfVotersWhoVoted().toString(),
+				multiElectionResultsAll.getDistPercentageOfVoted().toString(),
+				multiElectionResultsAll.getDistInvalidVotes().toString(),
+				multiElectionResultsAll.getDistPercentageOfInvalidVotes().toString(),
+				multiElectionResultsAll.getDistValidVotes().toString(),
+				multiElectionResultsAll.getDistPercentageOfValidVotes().toString(),
+				/* district */
+				/* result */
+				multiElectionResultsAll.getResId().toString(),
+				multiElectionResultsAll.getResPartyTitle(),
+				multiElectionResultsAll.getResVotes().toString(),
+				multiElectionResultsAll.getResPercentageOfAllVotes().toString()
+//				multiElectionResultsAll.getResNumberOfMandates().toString()
+				/* result */			
+				};
+	}
+	
+	public String[] request3ToStringArray(SingleElectionResultsAll singleElectionResultsAll) {
+		log.info("||--> was used." );
+		return new String[] { 
+				/* constituency */
+				singleElectionResultsAll.getConId().toString(),
+				singleElectionResultsAll.getConTitle(),
+				singleElectionResultsAll.getConNumberOfDistricts().toString(),
+				singleElectionResultsAll.getConNumberOfDistrictsPublishedResults().toString(),
+				singleElectionResultsAll.getConNumberOfVoters().toString(),
+				singleElectionResultsAll.getConNumberOfVotersWhoVote().toString(),
+				singleElectionResultsAll.getConPercentageOfVotersWhoVote().toString(),
+				singleElectionResultsAll.getConNumberOfInvalidVotes().toString(),
+				singleElectionResultsAll.getConPercentageOfInvalidVotes().toString(),
+				singleElectionResultsAll.getConNumberOfValidVotes().toString(),
+				singleElectionResultsAll.getConPercentageOfValidVotes().toString(),
+				/* constituency */
+				/* district */
+				singleElectionResultsAll.getDistId().toString(),
+				singleElectionResultsAll.getDistTitle(),
+				singleElectionResultsAll.getDistNumberOfVoters().toString(),
+				singleElectionResultsAll.getDistVoted().toString(),
+				singleElectionResultsAll.getDistPercentageOfVoted().toString(),
+				singleElectionResultsAll.getDistInvalidVotes().toString(),
+				singleElectionResultsAll.getDistPercentageOfInvalidVotes().toString(),
+				singleElectionResultsAll.getDistValidVotes().toString(),
+				singleElectionResultsAll.getDistPercentageOfValidVotes().toString(),
+				/* district */
+				/* result */
+				singleElectionResultsAll.getResCandidateId().toString(),
+				singleElectionResultsAll.getResCandidateFirstname(),
+				singleElectionResultsAll.getResCandidateSurname(),
+				singleElectionResultsAll.getResParty(),
+				singleElectionResultsAll.getResVoted().toString(),
+				singleElectionResultsAll.getResPercentageOfValidVotes().toString(),
+				singleElectionResultsAll.getResPercentageOfAllVotes().toString(),
+				singleElectionResultsAll.getResConsId().toString()
+				/* result */			
+				};
+	}
+
+	public String[] request2ToStringArray(SingleElectionResultsAll singleElectionResultsAll) {
 		log.info("||--> was used." );
 		return new String[] { singleElectionResultsAll.getConTitle(), singleElectionResultsAll.getDistTitle(),
 				singleElectionResultsAll.getResCandidateFirstname(), singleElectionResultsAll.getResCandidateSurname(),
@@ -122,7 +445,7 @@ public class DownloadResultsService {
 				singleElectionResultsAll.getResPercentageOfAllVotes().toString() };
 	}
 
-	public String[] singleElectionConstituencyToStringArray(SingleElectionConstituency singleConstituencyResult) {
+	public String[] request1ToStringArray(SingleElectionConstituency singleConstituencyResult) {
 		log.info("||--> was used." );
 		return new String[] { singleConstituencyResult.getTitle(),
 				singleConstituencyResult.getNumberOfDistricts().toString(),
@@ -150,5 +473,8 @@ public class DownloadResultsService {
 		log.info("||--> Finished!" );
 		return stringWriter.toString();
 	}
-
+	
 }
+
+
+ 
--- src/main/resources/static/js/public/home_component.jsx
+++ src/main/resources/static/js/public/home_component.jsx
@@ -1,21 +1,32 @@
-var HomeComponent = React.createClass({
+var HomeComponent = React.createClass( {
     render: function() {
         return (
-                <div>
-                    <h1>Sveiki atvykę!</h1>
-                    
-                    <DownloadCSVContainer 
-                    buttonName="Atsisiųsti CSV" 
-                    request="1"
-                    fileName="test.csv"/>
-                    <br />
-                    <DownloadCSVContainer 
-                    buttonName="Atsisiųsti CSV 2" 
+            <div>
+                <h1>Sveiki atvykę!</h1>
+
+                <DownloadCSVContainer
+                    buttonName="Visi vienmandačių rezultatai"
+                    request="3"
+                    fileName="Visi vien.csv" />
+                <br />
+                <DownloadCSVContainer
+                    buttonName="Esminiai vienmandačių rezultatai"
                     request="2"
-                    fileName="other.csv"/>
-                    
-                    <p>test.csv -> excel2010 -> data -> From text -> delimited -> encoding utf-8 -> comma</p>
-                </div>
+                    fileName="Esminiai vien.csv" />
+                <br />
+                <DownloadCSVContainer
+                    buttonName="Visi daugiamandačių rezultatai"
+                    request="4"
+                    fileName="Visi daug.csv" />
+                <br />
+                <DownloadCSVContainer
+                    buttonName="Esminiai daugiamandačių rezultatai"
+                    request="1"
+                    fileName="Esminiai daug.csv" />
+                <br />
+
+                <p>test.csv -> excel2010 -> data -> From text -> delimited -> encoding utf-8 -> comma</p>
+            </div>
         )
     }
 });
